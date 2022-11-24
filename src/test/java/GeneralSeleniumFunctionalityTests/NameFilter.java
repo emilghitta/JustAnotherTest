0c0f551aac24651d39dbec75a44dff123ce9b9a2
emilghitta <emilghitta@gmail.com>
2022-11-25 00:22:40 +0200

    Updating nameFilterTest().
    - Removed before and after methods.
    - Extended the TestUtilities class inside the NameFilter class to inherit the WebDriver instance.

--- src/test/java/GeneralSeleniumFunctionalityTests/NameFilter.java
+++ src/test/java/GeneralSeleniumFunctionalityTests/NameFilter.java
@@ -1,42 +1,16 @@
 package GeneralSeleniumFunctionalityTests;
 
+import core.TestUtilities;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class NameFilter {
-    private WebDriver driver;
-
-    @Parameters({"browser"})
-    @BeforeMethod(alwaysRun = true)
-    private void setUP(String browser){
-        switch (browser.toUpperCase()){
-            case "FIREFOX":
-                System.setProperty("webdriver.geck.driver","src/main/java/resources/geckodriver.exe");
-                driver = new FirefoxDriver();
-                break;
-            case "CHROME":
-                System.setProperty("webdriver.chrome.driver","src/main/java/resources/chromedriver.exe");
-                driver = new ChromeDriver();
-                break;
-            default:
-                System.setProperty("webdriver.edge.driver","src/main/java/resources/msedgedriver.exe");
-                driver = new EdgeDriver();
-        }
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-    }
+public class NameFilter extends TestUtilities {
 
     @Parameters({"pageURL"})
     @Test
@@ -72,11 +46,6 @@ public class NameFilter {
             }
         }
         Assert.assertFalse("The invalid fruit name found is: " +invalidFruit ,failure);
-    }
-
-    @AfterMethod(alwaysRun = true)
-    private void tearDown(){
-        driver.quit();
     }
 
 }
